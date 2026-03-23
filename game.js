ce8cd15c30065fb7fda8282315c5ba0638ff8c69
eszterpalotas <eszter.palotas@innoveo.com>
2026-03-23 14:44:10 +0100

    add money

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 let counter = 0;
+let money = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
   const muffin = document.getElementById("muffin-photo");
@@ -9,6 +10,32 @@ function countClick() {
     counter++;
     let muffinCount = document.getElementById("click-counter");
     muffinCount.innerHTML = `${counter} muffin(s)`;
+    displayPopupMessage();
+    getMoneyCount();
     return muffinCount;
 }
+
+function displayPopupMessage() {
+    const isDivisibleByTen = Math.floor(counter / 10) * 10;
+    let popUpMessage = document.getElementById("pop-up-message");
+
+    if (counter == isDivisibleByTen) {
+         return popUpMessage.innerHTML = `Yay! You reached ${counter} muffins!`;
+    } else {
+        return popUpMessage.innerHTML = "";
+    }
+}
+
+function getMoneyCount() {
+    const isDivisibleByThree = Math.floor(counter / 3) * 3;
+    let moneyCount = document.getElementById("money-counter");
+
+    if (counter == isDivisibleByThree) {
+        money++; 
+        moneyCount.innerHTML = `${money}`;
+        return money;
+    } 
+}
+
+
 
